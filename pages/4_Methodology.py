a82cf78bb690fa38a6e2ee0ced505badd198d542
markteojunyi <126735033+markteojunyi@users.noreply.github.com>
2024-10-23 17:35:23 +0800

    initial commit_methodology
    Added the codes to populate this page as per project requirements

--- pages/4_Methodology.py
+++ pages/4_Methodology.py
@@ -4,3 +4,29 @@ from helper_function.stpassword import check_password
 
 if not check_password():  
     st.stop()
+
+    import streamlit as st
+
+# Set Streamlit page configuration
+st.set_page_config(page_title="Methodology", page_icon="🔍")
+
+# Set the title of the Streamlit app
+st.title("Methodology")
+
+# Section for Buy HDB Flat App
+st.markdown("**Buy HDB Flat**")
+
+# Add space using HTML line break
+st.markdown("<br>", unsafe_allow_html=True)
+
+# Insert Image 1
+st.image("Images/Methodology_BuyHDBFlat.png", caption="Buy HDB Flat app flowchart", use_column_width=True)
+
+# Section for Resale Price App
+st.markdown("**Resale Price App**")
+
+# Add space using HTML line break
+st.markdown("<br>", unsafe_allow_html=True)
+
+# Insert Image 2
+st.image("Images/Methodology_Resaleprice.png", caption="Resale Price App flowchart", use_column_width=True)
