f1f1d03e7308b0875805901c4a4abde4addb64f8
markteojunyi <126735033+markteojunyi@users.noreply.github.com>
2024-10-23 17:51:28 +0800

    Add disclaimer to main app

--- BuyHDBFlat.py
+++ BuyHDBFlat.py
@@ -9,6 +9,20 @@ from helper_function.stpassword import check_password
 
 if not check_password():  
     st.stop()
+    
+# Create an expander for the disclaimer
+with st.expander("IMPORTANT NOTICE: Click to expand/disclose", expanded=False):
+    st.write("""
+    This web application is a prototype developed for educational purposes only. 
+    The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, 
+    especially those related to financial, legal, or healthcare matters.
+
+    Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. 
+    You assume full responsibility for how you use any generated output.
+
+    Always consult with qualified professionals for accurate and personalized advice.
+    """)
+
 
 # Set Streamlit page configuration
 st.set_page_config(page_title="HDB app", page_icon="🏠")
